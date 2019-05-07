c067f51d966f26a698a5a71a1b9ccef3b2b44700
Emil Ståhl <emil.stahl@icloud.com>
2019-05-07 14:30:41 +0200

    Destructuring arguments

--- vidly/src/components/common/table.jsx
+++ vidly/src/components/common/table.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
-const table = props => {
-  const { columns, sortColumn, data, onSort } = props;
+const table = ({ columns, sortColumn, data, onSort }) => {
   return (
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
