614b45189a2cd1f9fed7c50387f46f8e6ebb1b78
Emil Ståhl <emil.stahl@icloud.com>
2019-05-20 14:06:41 +0200

    logout

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
+import Logout from "./components/Logout";
 import RegisterForm from "./components/registerForm";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
@@ -31,6 +32,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login/" component={LoginForm} />
+            <Route path="/logout/" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
