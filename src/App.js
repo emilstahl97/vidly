b373260ce473c054ba4849ba5b5125f9c0cfcf5a
Emil Ståhl <emil.stahl@icloud.com>
2019-05-21 11:03:49 +0200

    Extracted protectedRoute

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import Logout from "./components/logout";
+import ProtectedRoute from "./components/common/protectedRoute";
 import RegisterForm from "./components/registerForm";
 import auth from "./services/authService";
 import "react-toastify/dist/ReactToastify.css";
@@ -31,14 +32,7 @@ class App extends Component {
             <Route path="/register" component={RegisterForm} />
             <Route path="/login/" component={LoginForm} />
             <Route path="/logout/" component={Logout} />
-            <Route
-              path="/movies/:id"
-              render={props => {
-                if (!user) return <Redirect to="/login" />;
-                return <MovieForm {...props} />;
-              }}
-            />
-
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
             <Route
               path="/movies"
               render={props => <Movies {...props} user={this.state.user} />}
